103a548b868f2aa9dd889d5666df750e77506f38
trieu.nguyenkhac <148073540+trieunguyenkhac@users.noreply.github.com>
2026-05-03 22:48:34 +0700

    Add inferenceScore parameter to sendSensorData function

--- include/task_webserver.h
+++ include/task_webserver.h
@@ -15,6 +15,7 @@ extern String wifi_password;
 extern void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
 extern void parseWebSocketMessage(AsyncWebSocketClient *client, const String &message);
 extern void sendSensorDataToWebSocket(float temperature, float humidity);
+extern void sendSensorDataToWebSocket(float temperature, float humidity, float inferenceScore);
 extern void handleToggleDevice(const String &message);
 extern void handleLedControl(AsyncWebSocketClient *client, const String &message);
 extern void handleNeoControl(AsyncWebSocketClient *client, const String &message);
